08a8a92f8e942ee4989cb7aac15fff06e14306c1
Abdullah Al Fahim <50063518+fahimw@users.noreply.github.com>
2021-02-03 07:34:46 +0600

    map and Filter

--- map_filter.js
+++ map_filter.js
@@ -7,3 +7,13 @@ numbers.map(function(element, index, Array) {
 
 const result = numbers.map(x => x * x);
 console.log(result);
+
+const filterNumber = numbers.filter(function(elemet) {
+    const result = elemet > 4;
+    console.log(result);
+})
+console.log(filterNumber);
+const bigNumber = numbers.filter(x => x > 4);
+const findNumber = numbers.find(x => x > 4);
+console.log(bigNumber);
+console.log(findNumber);
